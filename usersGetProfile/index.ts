22601f306b514d2c30882fcc0ad79c4202adf5f9
antonio simoes <antonio.simoes@bjss.com>
2021-04-15 14:56:48 +0100

    refactor: 💡 reworks get-user-profile endpoint

--- usersGetProfile/index.ts
+++ usersGetProfile/index.ts
@@ -1,30 +1,50 @@
-import { Context, HttpRequest } from "@azure/functions";
+import { AzureFunction, Context, HttpRequest } from "@azure/functions";
 import * as Responsify from "../utils/responsify";
 import * as validation from "./validation";
 import { decodeToken } from "../utils/authentication";
 import * as persistence from "./persistence";
-import { SetupConnection, Validate } from "../utils/decorators";
+import { setupSQLConnection } from "../utils/connection";
 
-class UsersGetProfile {
-  @SetupConnection()
-  @Validate(validation.ValidateHeaders, "Invalid Headers")
-  static async httpTrigger(ctx: Context, req: HttpRequest): Promise<void> {
-    const token = req.headers.authorization;
-    const jwt = decodeToken(token);
-    const id = jwt.oid;
-
-    let result;
-
-    try {
-      result = await persistence.getProfile(id);
-    } catch (error) {
-      ctx.log.error(error);
-      ctx.res = Responsify.Internal();
-      return;
-    }
-
-    ctx.res = Responsify.Ok(result);
+const httpTrigger: AzureFunction = async function (
+  context: Context,
+  req: HttpRequest
+): Promise<void> {
+  try {
+    await setupSQLConnection();
+  } catch (error) {
+    context.log.error(error);
+    context.res = Responsify.Internal({
+      error: "Error establishing connection with the datasource.",
+    });
+    return;
   }
-}
+  context.log.info("Database connection established");
 
-export default UsersGetProfile.httpTrigger;
+  const validate = validation.ValidateHeaders(req.headers);
+  if (validate.error) {
+    context.log.error(validate.error);
+    context.res = Responsify.BadRequest({ error: "Invalid query parameters" });
+    return;
+  }
+  const token = req.headers.authorization;
+  const jwt = decodeToken(token);
+  const id = jwt.oid;
+
+  let result;
+  try {
+    result = await persistence.getProfile(id);
+  } catch (error) {
+    context.log.error(error);
+    context.res = Responsify.Internal();
+    return;
+  }
+
+  if (result) {
+    context.res = Responsify.Ok(result);
+    return;
+  }
+
+  context.res = Responsify.NotFound(null);
+};
+
+export default httpTrigger;
