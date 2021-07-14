a7ed6754da055b3c3f9314b2f8a0b465469fffd2
Diogo <diogo.teixeira@bjss.com>
2021-07-14 17:40:37 +0100

    feat: 🎸 add initial organisations & units

--- __external__/domain/seeds/1626172199310-load-initial-organisations.ts
+++ __external__/domain/seeds/1626172199310-load-initial-organisations.ts
@@ -14,16 +14,182 @@ export class loadInitialOrganisations1626172199310 extends BaseSeed {
   public async up(queryRunner: QueryRunner): Promise<any> {
     const data = [
       {
-        name: "Organisation Test",
-        acronym: "OTT",
+        name: "NOCRI/NIHR",
+        acronym: "NOCRI/NIHR",
         units: [
           {
-            name: "Unit A",
-            acronym: "UTA",
+            name: "NOCRI/NIHR",
+            acronym: "NOCRI/NIHR",
+            isShadow: true,
+          },
+        ],
+      },
+      {
+        name: "NHS Supply Chain",
+        acronym: "NHS-SC",
+        units: [
+          {
+            name: "NHS Supply Chain",
+            acronym: "NHS-SC",
+            isShadow: true,
+          },
+        ],
+      },
+      {
+        name: "NICE",
+        acronym: "NICE",
+        units: [
+          {
+            name: "NICE",
+            acronym: "NICE",
+            isShadow: true,
+          },
+        ],
+      },
+      {
+        name: "Scottish Heath Technology Group",
+        acronym: "SHTG",
+        units: [
+          {
+            name: "Scottish Heath Technology Group",
+            acronym: "SHTG",
+            isShadow: true,
+          },
+        ],
+      },
+      {
+        name: "NHSE Specialised Commissioning",
+        acronym: "NHSE-SC",
+        units: [
+          {
+            name: "NHSE Specialised Commissioning",
+            acronym: "NHSE-SC",
+            isShadow: true,
+          },
+        ],
+      },
+      {
+        name: "Life Sciences Hub Wales",
+        acronym: "LSHW",
+        units: [
+          {
+            name: "Life Sciences Hub Wales",
+            acronym: "LSHW",
+            isShadow: true,
+          },
+        ],
+      },
+      {
+        name: "Department for International Trade",
+        acronym: "DIT",
+        units: [
+          {
+            name: "Department for International Trade",
+            acronym: "DIT",
+            isShadow: true,
+          },
+        ],
+      },
+      {
+        name: "Health Technology Wales",
+        acronym: "HTW",
+        units: [
+          {
+            name: "Health Technology Wales",
+            acronym: "HTW",
+            isShadow: true,
+          },
+        ],
+      },
+      {
+        name: "MHRA",
+        acronym: "MHRA",
+        units: [
+          {
+            name: "Devices Regulatory Group",
+            acronym: "DRG",
           },
           {
-            name: "Unit B",
-            acronym: "UTB",
+            name: "Devices Information & Operations Group",
+            acronym: "DIOG",
+          },
+          {
+            name: "Devices Safety & Surveillance Group",
+            acronym: "DSSG",
+          },
+          {
+            name: "Software Group",
+            acronym: "SG",
+          },
+        ],
+      },
+      {
+        name: "AHSN Network",
+        acronym: "AHSN",
+        units: [
+          {
+            name: "NENC",
+            acronym: "NENC",
+          },
+          {
+            name: "EMAHSN",
+            acronym: "EMAHSN",
+          },
+          {
+            name: "West of England",
+            acronym: "WoE",
+          },
+          {
+            name: "Eastern",
+            acronym: "EAST",
+          },
+          {
+            name: "NWC",
+            acronym: "NWC",
+          },
+          {
+            name: "WMAHSN",
+            acronym: "WMAHSN",
+          },
+          {
+            name: "WEAHSN",
+            acronym: "WEAHSN",
+          },
+          {
+            name: "HIN",
+            acronym: "HIN",
+          },
+          {
+            name: "Oxford",
+            acronym: "Oxford",
+          },
+          {
+            name: "UCLP",
+            acronym: "UCLP",
+          },
+          {
+            name: "YHAHSN",
+            acronym: "YHAHSN",
+          },
+          {
+            name: "HInM",
+            acronym: "HInM",
+          },
+          {
+            name: "KSS",
+            acronym: "KSS",
+          },
+          {
+            name: "Wessex",
+            acronym: "Wessex",
+          },
+          {
+            name: "ICHP",
+            acronym: "ICHP",
+          },
+          {
+            name: "SWAHSN",
+            acronym: "SWAHSN",
           },
         ],
       },
