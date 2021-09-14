c9794c75feea182a6ec122f01f94a2d2229f8fda
Diogo <diogo.teixeira@bjss.com>
2021-09-14 16:30:48 +0100

    fix: 🐛 change reason from required to optional

--- innovatorsArchiveInnovation/validation/index.ts
+++ innovatorsArchiveInnovation/validation/index.ts
@@ -1,8 +1,8 @@
 import Joi = require("joi");
 
 const payloadSchema = Joi.object({
-  reason: Joi.string().required(),
-}).required();
+  reason: Joi.string().optional(),
+}).optional();
 
 export const ValidatePayload = (data: object): any => {
   return payloadSchema.validate(data);
