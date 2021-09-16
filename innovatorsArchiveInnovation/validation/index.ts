18ec405961860edf8fde62bffa264f0eaf92c56e
Shalini Nayak <Shalini.Nayak1@nhs.net>
2021-09-16 16:40:04 +0530

    Fixed archival reason to be null or empty

--- innovatorsArchiveInnovation/validation/index.ts
+++ innovatorsArchiveInnovation/validation/index.ts
@@ -1,7 +1,7 @@
 import Joi = require("joi");
 
 const payloadSchema = Joi.object({
-  reason: Joi.string().required(),
+  reason: Joi.string().allow(null).allow("").optional(),
 }).required();
 
 export const ValidatePayload = (data: object): any => {
