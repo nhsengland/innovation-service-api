be69fa118d32a31495feb6dc0db49447701a7f58
Shalini Nayak <Shalini.Nayak1@nhs.net>
2021-10-13 19:26:32 +0530

    Send Action email to Innovator based on preference, test cases

---
+++ __tests__/notificationsGetEmailTypes/persistence.spec.ts
@@ -0,0 +1,45 @@
+import * as persistence from "../../notificationsGetEmailTypes/persistence";
+import * as typeorm from "typeorm";
+import { CustomContext } from "../../utils/types";
+import * as dotenv from "dotenv";
+import * as path from "path";
+import { NotificationService } from "@services/services/Notification.service";
+describe("[notificationsGetEmailTypes] Persistence suite", () => {
+    beforeAll(() => {
+        dotenv.config({
+            path: path.resolve(__dirname, "../.environment"),
+        });
+    });
+
+    describe("getEmailNotificationPreferences", () => {
+        it("should find all email notification preferences for a user", async () => {
+            // Arrange
+            spyOn(typeorm, "getRepository");
+            spyOn(typeorm, "getConnection");
+            const spy = spyOn(
+                NotificationService.prototype,
+                "getEmailNotificationPreferences"
+            ).and.returnValue([
+                { id: "NotificationType", isSubscribed: false }
+            ]);
+
+            const ctx = {
+                auth: {
+                    requestUser: {
+                        id: ":userId"
+                    },
+                },
+                services: {
+                    NotificationService: new NotificationService(),
+                },
+            };
+            // Act
+            await persistence.getEmailNotificationPreferences(
+                ctx as CustomContext
+            );
+            //Assert
+            expect(spy).toHaveBeenCalled();
+        });
+
+    });
+});
