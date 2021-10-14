9e039406d04bf3ebb9c3e998d924597fb18de595
Jyoti Jaiswal <jyoti.jaiswal@england.nhs.uk>
2021-10-14 17:38:22 +0530

    added new test for sidebar filters

---
+++ __tests__/accessorsGetAllActionsAdvance/index.spec.ts
@@ -0,0 +1,154 @@
+/* eslint-disable */
+import { AccessorOrganisationRole } from "@services/index";
+import {
+  createHttpTrigger, runStubFunctionFromBindings
+} from "stub-azure-function-context";
+import accessorsGetAllActionsAdvance from "../../accessorsGetAllActionsAdvance";
+import * as persistence from "../../accessorsGetAllActionsAdvance/persistence";
+import * as authentication from "../../utils/authentication";
+import * as connection from "../../utils/connection";
+import * as service_loader from "../../utils/serviceLoader";
+
+jest.mock("../../utils/logging/insights", () => ({
+  start: () => { },
+  getInstance: () => ({
+    startOperation: () => ({
+      operation: {
+        parentId: ":parent_id",
+      },
+    }),
+    wrapWithCorrelationContext: (func) => {
+      return func;
+    },
+    defaultClient: {
+      trackTrace: () => { },
+      trackRequest: () => { },
+      flush: () => { },
+    },
+  }),
+}));
+
+const dummy = {
+  services: {
+    OrganisationService: {
+      findUserOrganisations: () => [
+        { role: AccessorOrganisationRole.QUALIFYING_ACCESSOR, organisation: { id: ':orgId' } },
+      ],
+    },
+  },
+};
+
+describe("[HttpTrigger] accessorsGetAllActionsAdvance Suite", () => {
+  describe("Function Handler", () => {
+    afterEach(() => {
+      jest.resetAllMocks();
+    });
+
+    it("fails when connection is not established", async () => {
+      spyOn(authentication, 'decodeToken').and.returnValue({ oid: ':oid' });
+      spyOn(connection, "setupSQLConnection").and.throwError(
+        "Error establishing connection with the datasource."
+      );
+
+      const { res } = await mockedRequestFactory({});
+      expect(res.status).toBe(500);
+      expect(res.body.error).toBeDefined();
+      expect(res.body.error).toBe(
+        "Error establishing connection with the datasource."
+      );
+    });
+
+    it("Should return 200 when Innovation Actions is found", async () => {
+      spyOn(connection, "setupSQLConnection").and.returnValue(null);
+      spyOn(service_loader, "loadAllServices").and.returnValue(dummy.services);
+      spyOn(authentication, "decodeToken").and.returnValue({
+        oid: ":accessor_id",
+      });
+      spyOn(persistence, "findAllByAccessorAdavance").and.returnValue([
+        { id: ":action_id" },
+      ]);
+
+      const { res } = await mockedRequestFactory({});
+      expect(res.status).toBe(200);
+    });
+
+    it("Should return 403 when accessor has an invalid role", async () => {
+      const services = {
+        OrganisationService: {
+          findUserOrganisations: () => [{ role: "other" }],
+        },
+      };
+
+      spyOn(connection, "setupSQLConnection").and.returnValue(null);
+      spyOn(service_loader, "loadAllServices").and.returnValue(services);
+      spyOn(authentication, "decodeToken").and.returnValue({
+        oid: "test_accessor_id",
+      });
+      spyOn(persistence, "findAllByAccessorAdavance").and.returnValue([
+        { id: ":action_id" },
+      ]);
+
+      const { res } = await mockedRequestFactory({
+        headers: { authorization: ":access_token" },
+      });
+      expect(res.status).toBe(403);
+    });
+
+    it("Should throw error when oid is different from accessorId", async () => {
+      spyOn(connection, "setupSQLConnection").and.returnValue(null);
+      spyOn(service_loader, "loadAllServices").and.returnValue(dummy.services);
+      spyOn(authentication, "decodeToken").and.returnValue({
+        oid: "test",
+      });
+      spyOn(persistence, "findAllByAccessorAdavance").and.returnValue([
+        { id: ":action_id" },
+      ]);
+
+      const { res } = await mockedRequestFactory({
+        headers: { authorization: ":access_token" },
+      });
+      expect(res.status).toBe(403);
+    });
+
+    it("Should handle error persistence return error", async () => {
+      spyOn(connection, "setupSQLConnection").and.returnValue(null);
+      spyOn(service_loader, "loadAllServices").and.returnValue(dummy.services);
+      spyOn(authentication, "decodeToken").and.returnValue({
+        oid: ":accessor_id",
+      });
+      spyOn(persistence, "findAllByAccessorAdavance").and.throwError(
+        "Error."
+      );
+
+      const { res } = await mockedRequestFactory({
+        headers: { authorization: ":access_token" },
+      });
+      expect(res.status).toBe(500);
+    });
+  });
+});
+
+async function mockedRequestFactory(data?: any) {
+  return runStubFunctionFromBindings(
+    accessorsGetAllActionsAdvance,
+    [
+      {
+        type: "httpTrigger",
+        name: "req",
+        direction: "in",
+        data: createHttpTrigger(
+          "GET",
+          "http://nhse-i-aac/api/accessors/{userId}/actions/advance",
+          { ...data.headers }, // headers
+          {
+            userId: ":accessor_id"
+          }, // pathparams
+          {}, // payload/body
+          { innovationStatus: '', innovationSection: '', name: '', skip: 0, take: 20, order: '{"name": "asc"}' } // querystri
+        ),
+      },
+      { type: "http", name: "res", direction: "out" },
+    ],
+    new Date()
+  );
+}
