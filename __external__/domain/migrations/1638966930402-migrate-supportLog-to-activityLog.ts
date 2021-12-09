dab0f326525e00fd4c811d9dea0a7f6fa9ced939
Shalini Nayak <Shalini.Nayak1@nhs.net>
2021-12-09 22:23:56 +0530

    Added activity log migration for additional activities

--- __external__/domain/migrations/1638966930402-migrate-supportLog-to-activityLog.ts
+++ __external__/domain/migrations/1638966930402-migrate-supportLog-to-activityLog.ts
@@ -3,6 +3,12 @@ import { MigrationInterface, QueryRunner } from "typeorm";
 export class migrateSupportLogToActivityLog1638966930402
   implements MigrationInterface {
   public async up(queryRunner: QueryRunner): Promise<void> {
+
+    // Move Activity log data into a temp table
+    await queryRunner.query(
+      `SELECT * INTO #TempAcitivityLog FROM dbo.activity_log`
+    );
+
     // Insert support logs of type ACCESSOR_SUGGESTION into ActivityLog table (activity - ORGANISATION_SUGGESTION & type - SUPPORT)
     await queryRunner.query(
       `INSERT INTO dbo.activity_log (created_at, created_by, updated_at, updated_by, deleted_at, innovation_id, param, activity, type)
@@ -42,9 +48,102 @@ export class migrateSupportLogToActivityLog1638966930402
         where SL1.type = 'STATUS_UPDATE' 
         and not exists (select 1 from dbo.activity_log AL where AL.created_at = SL1.created_at and AL.created_by = SL1.created_by and AL.activity = 'SUPPORT_STATUS_UPDATE')`
     );
+
+    // Insert logs into ActivityLog table (activity - INNOVATION_CREATION & type - INNOVATION_MANAGEMENT)
+    await queryRunner.query(
+      ` INSERT INTO dbo.activity_log (created_at, created_by, innovation_id, param, activity, type)
+        SELECT inn.created_at, inn.created_by, inn.id, '{"actionUserId":"' + inn.created_by +'"}', 'INNOVATION_CREATION', 'INNOVATION_MANAGEMENT' FROM innovation inn
+        Where not exists (select 1 from dbo.activity_log AL where AL.innovation_id = inn.id and AL.activity = 'INNOVATION_CREATION')
+        `
+    );
+
+    // Insert logs into ActivityLog table (activity - INNOVATION_SUBMISSION & type - NEEDS_ASSESSMENT)
+    await queryRunner.query(
+      ` INSERT INTO dbo.activity_log (created_at, created_by, innovation_id, param, activity, type)
+        SELECT inn.submitted_at, inn.created_by, inn.id, '{"actionUserId":"' + inn.created_by +'"}', 'INNOVATION_SUBMISSION', 'NEEDS_ASSESSMENT' FROM innovation inn
+        WHERE inn.submitted_at is not null
+        and not exists (select 1 from dbo.activity_log AL where AL.innovation_id = inn.id and AL.activity = 'INNOVATION_SUBMISSION')  `
+    );
+
+    // Insert logs into ActivityLog table (activity - NEEDS_ASSESSMENT_START & type - NEEDS_ASSESSMENT)
+    await queryRunner.query(
+      ` INSERT INTO dbo.activity_log (created_at, created_by, innovation_id, param, activity, type)
+        SELECT IA.created_at, IA.created_by, IA.innovation_id, '{"actionUserId":"' + IA.assign_to_id +'", "assessmentId":"' + convert(nvarchar(50), IA.id) + '"}', 
+        'NEEDS_ASSESSMENT_START', 'NEEDS_ASSESSMENT'
+        FROM innovation_assessment IA 
+        WHERE not exists (select 1 from dbo.activity_log AL where AL.innovation_id = IA.innovation_id and AL.activity = 'NEEDS_ASSESSMENT_START')`
+    );
+
+    // Insert logs into ActivityLog table (activity - NEEDS_ASSESSMENT_COMPLETED & type - NEEDS_ASSESSMENT)
+    await queryRunner.query(
+      ` INSERT INTO dbo.activity_log (created_at, created_by, innovation_id, param, activity, type)
+        SELECT IA.finished_at, IA.assign_to_id, IA.innovation_id, '{"actionUserId":"' + IA.assign_to_id +'", "assessmentId":"' + convert(nvarchar(50), IA.id) + '"}', 
+        'NEEDS_ASSESSMENT_COMPLETED', 'NEEDS_ASSESSMENT'
+        FROM innovation_assessment IA where IA.finished_at is not null
+        and not exists (select 1 from dbo.activity_log AL where AL.innovation_id = IA.innovation_id and AL.activity = 'NEEDS_ASSESSMENT_COMPLETED')`
+    );
+
+    // Insert logs into ActivityLog table (activity - COMMENT_CREATION & type - COMMENTS)
+    await queryRunner.query(
+      ` INSERT INTO dbo.activity_log (created_at, created_by, innovation_id, param, activity, type)
+  SELECT com.created_at, com.created_by, com.innovation_id,  
+	'{"actionUserId":"' + com.created_by +'", "comment":{"id": "'+ convert(nvarchar(50), com.id) +'", "value": "' +  com.message +'"}}',
+	 'COMMENT_CREATION', 'COMMENTS'
+  FROM comment com
+  where com.reply_to_id is null 
+  and not exists (select 1 from dbo.activity_log AL where AL.created_at = com.created_at and AL.innovation_id = com.innovation_id and AL.created_by = com.created_by and AL.activity = 'COMMENT_CREATION' )`
+    );
+
+    // Insert logs into ActivityLog table (activity - ACTION_CREATION & type - ACTIONS)
+    await queryRunner.query(
+      ` INSERT INTO dbo.activity_log (created_at, created_by, innovation_id, param, activity, type)
+      SELECT IA.created_at, IA.created_by, ISec.innovation_id,  
+      '{"actionUserId":"' + IA.created_by +'","sectionId":"' + ISec.section + '","actionId":"' + convert(varchar(50), IA.id) + '", "comment":{"value": "' +  IA.description +'"}}',
+       'ACTION_CREATION', 'ACTIONS'
+      FROM innovation_action IA INNER JOIN innovation_section ISec ON IA.innovation_section_id = ISec.id
+      where not exists (select 1 from dbo.activity_log AL where AL.created_at = IA.created_at and AL.innovation_id = ISec.innovation_id and AL.created_by = IA.created_by and AL.activity = 'ACTION_CREATION' ) `
+    );
+
+    // Insert logs into ActivityLog table (activity - ACTION_STATUS_COMPLETED_UPDATE & type - ACTIONS)
+    await queryRunner.query(
+      ` INSERT INTO dbo.activity_log (created_at, created_by, innovation_id, param, activity, type)
+      SELECT IA.updated_at, IA.updated_by, ISec.innovation_id,  
+      '{"actionUserId":"' + IA.updated_by +'","actionId":"' + convert(varchar(50), IA.id) + '","comment":{"id": "'+ convert(varchar(50), com.id) +'","value": "' +  com.message +'"}}',
+       'ACTION_STATUS_COMPLETED_UPDATE', 'ACTIONS'
+      FROM innovation_action IA INNER JOIN innovation_section ISec ON IA.innovation_section_id = ISec.id
+      LEFT OUTER JOIN comment com ON IA.id = com.innovation_action_id
+      WHERE IA.status = 'COMPLETED' AND dateadd(millisecond, -datepart(millisecond, IA.updated_at), IA.updated_at)  = dateadd(millisecond, -datepart(millisecond, com.created_at), com.created_at) AND IA.updated_by = com.created_by
+      AND NOT EXISTS (select 1 from dbo.activity_log AL where AL.created_at = IA.updated_at and AL.innovation_id = ISec.innovation_id and AL.created_by = IA.updated_by and AL.activity = 'ACTION_STATUS_COMPLETED_UPDATE' ) `
+    );
+
+    // Insert logs into ActivityLog table (activity - ACTION_STATUS_DECLINED_UPDATE & type - ACTIONS)
+    await queryRunner.query(
+      `  INSERT INTO dbo.activity_log (created_at, created_by, innovation_id, param, activity, type)
+      SELECT IA.updated_at, IA.updated_by, ISec.innovation_id,  
+      '{"actionUserId":"' + IA.updated_by +'","actionId":"' + convert(varchar(50), IA.id) + '", "comment":{"id": "'+ convert(varchar(50), com.id) +'","value": "' +  com.message +'"}}',
+       'ACTION_STATUS_DECLINED_UPDATE', 'ACTIONS'
+      FROM innovation_action IA INNER JOIN innovation_section ISec ON IA.innovation_section_id = ISec.id
+      LEFT OUTER JOIN comment com ON IA.id = com.innovation_action_id
+      WHERE IA.status = 'DECLINED' AND dateadd(millisecond, -datepart(millisecond, IA.updated_at), IA.updated_at)  = dateadd(millisecond, -datepart(millisecond, com.created_at), com.created_at) AND IA.updated_by = com.created_by
+      AND NOT EXISTS (select 1 from dbo.activity_log AL where AL.created_at = IA.updated_at and AL.innovation_id = ISec.innovation_id and AL.created_by = IA.updated_by and AL.activity = 'ACTION_STATUS_DECLINED_UPDATE' )`
+    );
   }
 
   public async down(queryRunner: QueryRunner): Promise<void> {
-    // irreversible
+
+    await queryRunner.query(
+      `DELETE FROM dbo.activity_log;`
+    );
+    
+    await queryRunner.query(
+      `INSERT INTO dbo.activity_log 
+      SELECT created_at, created_by, updated_at, updated_by, deleted_at, id, innovation_id, type, activity, param  from #TempAcitivityLog
+      `
+    );
+
+    await queryRunner.query(
+      `DROP TABLE IF EXISTS  #TempAcitivityLog`
+    );
+
   }
 }
