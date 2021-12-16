fc962048f4df9016621642d8ce29f92ad299554f
Shalini Nayak <Shalini.Nayak1@nhs.net>
2021-12-16 14:02:14 +0530

    Seed script to add admin user and its roles

--- __external__/domain/seeds/1639572757510-load-initial-service-roles.ts
+++ __external__/domain/seeds/1639572757510-load-initial-service-roles.ts
@@ -1,6 +1,6 @@
 /*eslint @typescript-eslint/no-unused-vars: ["off", { "varsIgnorePattern": "[qQ]ueryRunner" }]*/
 
-import { Role } from "@domain/index";
+import { Role, ServiceRole, User, UserType } from "@domain/index";
 import { QueryRunner } from "typeorm";
 
 import { BaseSeed } from "../tools/bases/base-seed";
@@ -9,7 +9,20 @@ export class loadInitialServiceRoles1639572757510 extends BaseSeed {
   public name = "loadInitialServiceRoles1639572757510";
 
   public async up(queryRunner: QueryRunner): Promise<any> {
+    const adminUserId = process.env.ADMIN_OID;
+    const userRepo = this.getRepository(User);
     const roleRepo = this.getRepository(Role);
+    const serviceRoleRepo = this.getRepository(ServiceRole);
+
+    // Add admin user
+    const userObj = User.new({
+      id: adminUserId,
+      type: UserType.ADMIN,
+      createdBy: "seed",
+      updatedBy: "seed",
+    });
+
+    await userRepo.save(userObj);
 
     // Seed roles
     const roles = [
@@ -35,11 +48,28 @@ export class loadInitialServiceRoles1639572757510 extends BaseSeed {
 
       await roleRepo.save(roleObj);
     }
+
+    // Assign service roles to Admin User
+    const serviceRoles = await roleRepo.find();
+
+    for (let roleIdx = 0; roleIdx < serviceRoles.length; roleIdx++) {
+      const curr = serviceRoles[roleIdx];
+
+      const srObj = ServiceRole.new({
+        user: adminUserId,
+        role: curr.id,
+        createdBy: "seed",
+        updatedBy: "seed",
+      });
+
+      await serviceRoleRepo.save(srObj);
+    }
   }
 
   public async down(queryRunner: QueryRunner): Promise<any> {
     const query = this.getConnection().createQueryBuilder().delete();
 
     await query.from(Role).execute();
+    await query.from(ServiceRole).execute();
   }
 }
