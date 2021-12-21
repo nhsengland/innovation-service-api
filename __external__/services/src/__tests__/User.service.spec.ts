8de4e52f9dc2a33da61ec2550cfd5c1a075d8447
antonio simoes <antonio.simoes@bjss.com>
2021-12-21 09:21:28 +0000

    test: 💍 fixes unit tests on user.service

--- __external__/services/src/__tests__/User.service.spec.ts
+++ __external__/services/src/__tests__/User.service.spec.ts
@@ -88,8 +88,6 @@ describe("User Service Suite", () => {
     await query.from(NotificationPreference).execute();
     await query.from(OrganisationUnitUser).execute();
     await query.from(OrganisationUser).execute();
-    await query.from(UserRole).execute();
-    await query.from(Role).execute();
     await query.from(User).execute();
   });
 
