7400ae9b96689e030a3d8ff4c9f367f3c1d50950
antonio simoes <antonio.simoes@bjss.com>
2021-12-29 12:47:04 +0000

    fixes cosmosdb connector

--- utils/connection.ts
+++ utils/connection.ts
@@ -7,29 +7,29 @@ let isSQLConnected = false;
 
 dotenv.config();
 
-// export const setupCosmosDb = async () => {
-//   if (!isCosmosConnected) {
-//     const connectionString = `mongodb://${
-//       process.env.COSMOSDB_ACCOUNT
-//     }:${escape(process.env.COSMOSDB_KEY)}@${process.env.COSMOSDB_HOST}:${
-//       process.env.COSMOSDB_PORT
-//     }/${process.env.COSMOSDB_DB}?ssl=true&retryWrites=false`;
-//     await mongoose.connect(connectionString, {
-//       dbName: process.env.COSMOS_DB_NAME,
-//     });
-//     isCosmosConnected = true;
-//   }
-// };
-
 export const setupCosmosDb = async () => {
   if (!isCosmosConnected) {
-    const connectionString = `mongodb://localhost`;
+    const connectionString = `mongodb://${
+      process.env.COSMOSDB_ACCOUNT
+    }:${escape(process.env.COSMOSDB_KEY)}@${process.env.COSMOSDB_HOST}:${
+      process.env.COSMOSDB_PORT
+    }/${process.env.COSMOSDB_DB}?ssl=true&retryWrites=false`;
     await mongoose.connect(connectionString, {
-      dbName: process.env.COSMOSDB_DB,
+      dbName: process.env.COSMOS_DB_NAME,
     });
     isCosmosConnected = true;
   }
 };
+
+// export const setupCosmosDb = async () => {
+//   if (!isCosmosConnected) {
+//     const connectionString = `mongodb://localhost`;
+//     await mongoose.connect(connectionString, {
+//       dbName: process.env.COSMOSDB_DB,
+//     });
+//     isCosmosConnected = true;
+//   }
+// };
 
 export const closeTestsCosmosDb = async () => {
   if (isCosmosConnected) {
