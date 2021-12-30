8542ffe20eb1c8220360eccee2e0a83f22dae9c7
antonio simoes <antonio.simoes@bjss.com>
2021-12-30 17:08:04 +0000

    creates TTL index cosmos compatible

--- schemas/TTL2ls.ts
+++ schemas/TTL2ls.ts
@@ -1,23 +1,29 @@
 import * as mongoose from "mongoose";
 
-const TTL2lsSchema = new mongoose.Schema({
-  id: mongoose.Schema.Types.ObjectId,
-  userId: mongoose.Schema.Types.String,
-  code: mongoose.Schema.Types.String,
-  eventType: {
-    type: String,
-    enum: [
-      "LOGIN",
-      "ADMIN_HEALTH",
-      "ADMIN_CREATE_USER",
-      "ADMIN_LOCK_USER",
-      "ADMIN_UNLOCK_USER",
-    ],
-    default: "LOGIN",
+const ttl = process.env.TTL2LS ? parseInt(process.env.TTL2LS) : 300;
+
+const TTL2lsSchema = new mongoose.Schema(
+  {
+    id: mongoose.Schema.Types.ObjectId,
+    userId: mongoose.Schema.Types.String,
+    code: mongoose.Schema.Types.String,
+    eventType: {
+      type: String,
+      enum: [
+        "LOGIN",
+        "ADMIN_HEALTH",
+        "ADMIN_CREATE_USER",
+        "ADMIN_LOCK_USER",
+        "ADMIN_UNLOCK_USER",
+      ],
+      default: "LOGIN",
+    },
+    validatedAt: { type: Date },
   },
-  createdAt: { type: Date, expires: process.env.TTL2LS || 300 },
-  validatedAt: { type: Date },
-});
+  { timestamps: true }
+);
+
+TTL2lsSchema.index({ createdAt: 1 }, { expireAfterSeconds: ttl });
 
 export const TTL2ls =
   mongoose.models.TTL2ls || mongoose.model("TTL2ls", TTL2lsSchema);
