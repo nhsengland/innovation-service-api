ea78bb04e2783524ef37ef7d275e9ab2a9ad098d
antonio simoes <antonio.simoes@bjss.com>
2022-02-09 11:41:02 +0000

    fixes domain unit tests entity clean up

--- __external__/domain/src/__tests__/InnovationAssessment.entity.spec.ts
+++ __external__/domain/src/__tests__/InnovationAssessment.entity.spec.ts
@@ -1,4 +1,4 @@
-import { ActivityLog } from "@domain/index";
+import { ActivityLog, UserRole } from "@domain/index";
 import { classToPlain } from "class-transformer";
 import { getConnection, getRepository, Repository } from "typeorm";
 import { getEntityColumnList } from "../../tools/helpers";
@@ -70,6 +70,7 @@ describe("Innovation Action Test Suite", () => {
     const query = getConnection(process.env.DB_TESTS_NAME)
       .createQueryBuilder()
       .delete();
+    await query.from(UserRole).execute();
     await query.from(ActivityLog).execute();
     await query.from(Organisation).execute();
     await query.from(Innovation).execute();
--- __external__/domain/src/__tests__/Notification.entity.spec.ts
+++ __external__/domain/src/__tests__/Notification.entity.spec.ts
@@ -1,4 +1,4 @@
-import { ActivityLog } from "@domain/index";
+import { ActivityLog, UserRole } from "@domain/index";
 import { classToPlain } from "class-transformer";
 import { getConnection, getRepository, Repository } from "typeorm";
 import { getEntityColumnList } from "../../tools/helpers";
@@ -19,6 +19,7 @@ describe("Notification Test Suite", () => {
     const query = getConnection(process.env.DB_TESTS_NAME)
       .createQueryBuilder()
       .delete();
+    await query.from(UserRole).execute();
     await query.from(ActivityLog).execute();
     await query.from(Notification).execute();
     await query.from(User).execute();
