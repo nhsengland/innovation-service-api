77528a78780c69cb5eff24269d13b29e86548deb
Shalini Nayak <Shalini.Nayak1@nhs.net>
2022-03-09 18:00:37 +0530

    Unit test cases

---
+++ __tests__/adminsUserDetails/index.spec.ts
@@ -0,0 +1,123 @@
+/* eslint-disable */
+import { AccessorOrganisationRole, UserType } from "@services/index";
+import {
+    createHttpTrigger, runStubFunctionFromBindings
+} from "stub-azure-function-context";
+import adminsUserDetails from "../../adminsUserDetails";
+import * as persistence from "../../adminsUserDetails/persistence";
+import * as authentication from "../../utils/authentication";
+import * as connection from "../../utils/connection";
+import * as service_loader from "../../utils/serviceLoader";
+
+
+jest.mock("../../utils/logging/insights", () => ({
+    start: () => { },
+    getInstance: () => ({
+        startOperation: () => ({
+            operation: {
+                parentId: ":parent_id",
+            },
+        }),
+        wrapWithCorrelationContext: (func) => {
+            return func;
+        },
+        defaultClient: {
+            trackTrace: () => { },
+            trackRequest: () => { },
+            flush: () => { },
+        },
+    }),
+}));
+
+const dummy = {
+    services: {
+        UserService: {
+            getUser: () => ({
+                type: UserType.ADMIN,
+                serviceRoles: [{
+                    role: {
+                        name: "ADMIN"
+                    }
+                }]
+            }),
+        }
+    },
+    adminUser: 'test_admin_oid',
+    accessorId: "test_accessor_id",
+};
+
+describe("[HttpTrigger] adminsUserDetails Suite", () => {
+    describe("Function Handler", () => {
+        afterEach(() => {
+            jest.resetAllMocks();
+        });
+
+        it("fails when connection is not established", async () => {
+            jest.spyOn(authentication, "decodeToken").mockReturnValue({ oid: ":oid" });
+            jest.spyOn(connection, "setupSQLConnection").mockRejectedValue(
+                "Error establishing connection with the datasource."
+            );
+
+            const { res } = await mockedRequestFactory({});
+            expect(res.status).toBe(500);
+            expect(res.body.error).toBeDefined();
+            expect(res.body.error).toBe(
+                "Error establishing connection with the datasource."
+            );
+        });
+
+        it("Should return 200 on successful completion", async () => {
+            jest.spyOn(connection, "setupSQLConnection").mockResolvedValue(null);
+            jest.spyOn(service_loader, "loadAllServices").mockResolvedValue(dummy.services as any);
+            jest.spyOn(authentication, "decodeToken").mockReturnValue({ oid: dummy.adminUser });
+
+            jest.spyOn(persistence, "getUser").mockResolvedValue({
+                id: ":userId",
+                name: ":userName",
+            } as any);
+
+            const { res } = await mockedRequestFactory({});
+
+            expect(res.status).toBe(200);
+        });
+
+        it("Should handle error persistence return error", async () => {
+            jest.spyOn(connection, "setupSQLConnection").mockResolvedValue(null);
+            jest.spyOn(service_loader, "loadAllServices").mockResolvedValue(dummy.services as any);
+            jest.spyOn(authentication, "decodeToken").mockReturnValue({
+                oid: dummy.adminUser,
+            });
+            jest.spyOn(persistence, "getUser").mockRejectedValue(
+                "Error."
+            );
+
+            const { res } = await mockedRequestFactory({
+                headers: { authorization: ":access_token" },
+            });
+            expect(res.status).toBe(500);
+        });
+    });
+});
+
+async function mockedRequestFactory(data?: any) {
+    return runStubFunctionFromBindings(
+        adminsUserDetails,
+        [
+            {
+                type: "httpTrigger",
+                name: "req",
+                direction: "in",
+                data: createHttpTrigger(
+                    "GET",
+                    "http://nhse-i-aac/api/user-admin/users/{userId}",
+                    { ...data.headers }, // headers
+                    { userId: dummy.accessorId },
+                    null, // payload/body
+                    { model: "MINIMAL" }, // querystring
+                ),
+            },
+            { type: "http", name: "res", direction: "out" },
+        ],
+        new Date()
+    );
+}
---
+++ __tests__/adminsUserDetails/persistence.spec.ts
@@ -0,0 +1,46 @@
+import * as typeorm from "typeorm";
+import * as persistence from "../../adminsUserDetails/persistence";
+import { CustomContext } from "../../utils/types";
+import * as dotenv from "dotenv";
+import * as path from "path";
+import { AdminService } from "@services/services/Admin.service";
+describe("[adminsUserDetails] Persistence suite", () => {
+  beforeAll(() => {
+    dotenv.config({
+      path: path.resolve(__dirname, "../.environment"),
+    });
+  });
+  describe("adminsUserDetails", () => {
+    it("should get user details", async () => {
+      // Arrange
+      jest.spyOn(typeorm, "getRepository").mockImplementation(jest.fn());
+      jest.spyOn(typeorm, "getConnection").mockImplementation(
+        (connectionName: string) =>
+          // eslint-disable-next-line @typescript-eslint/no-empty-function
+          ({ close: () => {} } as typeorm.Connection)
+      );
+      const spy = jest
+        .spyOn(AdminService.prototype, "getUserDetails")
+        .mockResolvedValue({
+          id: ":userId",
+          name: ":userName",
+        } as any);
+
+      const ctx = {
+        services: {
+          AdminService: new AdminService(),
+        },
+        auth: {
+          requestUser: {
+            id: ":userId",
+            type: "ADMIN",
+          },
+        },
+      };
+      // Act
+      await persistence.getUser(ctx as CustomContext, "test", "MINIMAL");
+
+      expect(spy).toHaveBeenCalled();
+    });
+  });
+});
