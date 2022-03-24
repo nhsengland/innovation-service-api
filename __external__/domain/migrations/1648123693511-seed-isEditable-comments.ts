e446cf19e00230d6a99eb315057dd3ffb4583162
Shalini Nayak <Shalini.Nayak1@nhs.net>
2022-03-24 20:32:05 +0530

    Updated query to include accessors comments except status update comments

--- __external__/domain/migrations/1648123693511-seed-isEditable-comments.ts
+++ __external__/domain/migrations/1648123693511-seed-isEditable-comments.ts
@@ -6,10 +6,15 @@ export class seedIsEditableComments1648123693511 implements MigrationInterface {
       `   UPDATE dbo.comment SET is_editable = 1 
                 FROM dbo.comment com INNER JOIN dbo.[user] usr
                 ON com.user_id = usr.id 
-                LEFT OUTER JOIN dbo.organisation_user orgUsr ON usr.id = orgUsr.user_id
                 WHERE com.innovation_action_id IS NULL 
                 AND usr.type IN ('INNOVATOR', 'ACCESSOR')
-                AND orgUsr.role = 'ACCESSOR'`
+                AND com.id NOT IN (
+                  SELECT DISTINCT cmt.id
+                  FROM comment cmt INNER JOIN dbo.[user] usr ON cmt.user_id = usr.id
+                  JOIN innovation_support_history ish on CONVERT(VARCHAR(8),cmt.created_at,108) = CONVERT(VARCHAR(8),ish.created_at,108)
+                  AND ish.organisation_unit_id = cmt.organisation_unit_id
+                  WHERE usr.[type] = 'ACCESSOR'
+                  )`
     );
   }
 
