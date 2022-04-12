aa6128effa970088eb38212373b4d94fb06fc84e
Jyoti Jaiswal <jyoti.jaiswal@england.nhs.uk>
2022-04-12 21:10:48 +0530

    QA and A change organisation unit

---
+++ adminsChangeOrganisationUnit/index.ts
@@ -0,0 +1,54 @@
+import { HttpRequest } from "@azure/functions";
+import { ServiceRole, UserType } from "@services/index";
+import { SLSEventType } from "@services/types";
+import {
+  AllowedUserType,
+  AppInsights,
+  JwtDecoder,
+  ServiceRoleValidator,
+  SLSValidation,
+  SQLConnector,
+  Validator,
+  CosmosConnector,
+} from "../utils/decorators";
+import * as Responsify from "../utils/responsify";
+import { CustomContext, Severity } from "../utils/types";
+import * as persistence from "./persistence";
+import * as validation from "./validation";
+
+class AdminsUpdateOrganisationUnit {
+  @AppInsights()
+  @SQLConnector()
+  @Validator(validation.ValidatePayload, "body", "Invalid Payload")
+  @JwtDecoder(true)
+  @CosmosConnector()
+  @AllowedUserType(UserType.ADMIN)
+  @ServiceRoleValidator(ServiceRole.ADMIN)
+  @SLSValidation(SLSEventType.ADMIN_UPDATE_ORGANISATION_UNIT)
+  static async httpTrigger(
+    context: CustomContext,
+    req: HttpRequest
+  ): Promise<void> {
+    const body = req.body;
+    const organisationUnitId = req.params.organisationUnitId;
+
+    let result;
+    try {
+      result = await persistence.updateOrganisationUnit(
+        context,
+        organisationUnitId,
+        body.name,
+        body.acronym
+      );
+    } catch (error) {
+      context.logger(`[${req.method}] ${req.url}`, Severity.Error, { error });
+      context.log.error(error);
+      context.res = Responsify.ErroHandling(error);
+      return;
+    }
+
+    context.res = Responsify.Ok(result);
+  }
+}
+
+export default AdminsUpdateOrganisationUnit.httpTrigger;
---
+++ adminsValidateChangeUnit/index.ts
@@ -0,0 +1,39 @@
+import { HttpRequest } from "@azure/functions";
+import { UserType } from "@domain/index";
+import {
+  AllowedUserType,
+  AppInsights,
+  JwtDecoder,
+  ServiceRoleValidator,
+  SQLConnector,
+} from "../utils/decorators";
+import * as Responsify from "../utils/responsify";
+import { CustomContext, ServiceRole, Severity } from "../utils/types";
+import * as persistence from "./persistence";
+
+class AdminsValidateChangeUnit {
+  @AppInsights()
+  @SQLConnector()
+  @JwtDecoder(true)
+  @AllowedUserType(UserType.ADMIN)
+  @ServiceRoleValidator(ServiceRole.ADMIN, ServiceRole.SERVICE_TEAM)
+  static async httpTrigger(
+    context: CustomContext,
+    req: HttpRequest
+  ): Promise<void> {
+    const userId = req.params.userId;
+    let result;
+    try {
+      result = await persistence.changeUnitValidation(context, userId);
+    } catch (error) {
+      context.logger(`[${req.method}] ${req.url}`, Severity.Error, { error });
+      context.log.error(error);
+      context.res = Responsify.ErroHandling(error);
+      return;
+    }
+
+    context.res = Responsify.Ok(result);
+  }
+}
+
+export default AdminsValidateChangeUnit.httpTrigger;
