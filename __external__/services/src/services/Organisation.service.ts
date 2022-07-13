30b6df99ecfcd55ef29b87fc42eda66f25182f5c
dev <diogo.teixeira@bjss.com>
2022-07-13 21:50:27 +0100

    fix: na completed notification to qa

--- __external__/services/src/services/Organisation.service.ts
+++ __external__/services/src/services/Organisation.service.ts
@@ -554,7 +554,7 @@ export class OrganisationService extends BaseService<Organisation> {
           id: organisationUnitUser.id,
           name: b2cMap[organisationUser.user.externalId],
           role: organisationUser.role,
-          userId: organisationUser.user.id
+          userId: organisationUser.user.id,
         };
       });
 
